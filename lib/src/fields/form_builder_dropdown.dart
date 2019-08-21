c41e0b81cd476ff649d9650876d0fa9c251e84f0
Danvick Miller <danvickmiller@live.com>
2019-08-21 13:09:32 +0300

    Allow adding InputBorder on DropdownField

--- lib/src/fields/form_builder_dropdown.dart
+++ lib/src/fields/form_builder_dropdown.dart
@@ -31,7 +31,7 @@ class FormBuilderDropdown extends StatefulWidget {
     this.readOnly = false,
     this.decoration = const InputDecoration(),
     this.isExpanded = true,
-    this.isDense = false,
+    this.isDense = true,
     this.elevation = 8,
     this.iconSize = 24.0,
     this.hint,
@@ -99,38 +99,70 @@ class _FormBuilderDropdownState extends State<FormBuilderDropdown> {
         return InputDecorator(
           decoration: widget.decoration.copyWith(
             errorText: field.errorText,
-            border: InputBorder.none,
           ),
-          child: DropdownButton(
-            isExpanded: widget.isExpanded,
-            hint: widget.hint,
-            items: widget.items,
-            value: field.value,
-            style: widget.style,
-            isDense: widget.isDense,
-            disabledHint: field.value != null
-                ? Text("${field.value.toString()}")
-                : widget.disabledHint,
-            elevation: widget.elevation,
-            iconSize: widget.iconSize,
-            icon: widget.icon,
-            iconDisabledColor: widget.iconDisabledColor,
-            iconEnabledColor: widget.iconEnabledColor,
-            underline: widget.underline,
-            onChanged: _readOnly
-                ? null
-                : (value) {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                    field.didChange(value);
-                    if (widget.onChanged != null) widget.onChanged(value);
-                  },
-            //TODO: add icon, enabledColor, disabledColor
-            /*icon: widget.icon,
-            iconEnabledColor: widget.iconEnabledColor,
-            iconDisabledColor: widget.iconDisabledColor,*/
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              isExpanded: widget.isExpanded,
+              hint: widget.hint,
+              items: widget.items,
+              value: field.value,
+              style: widget.style,
+              isDense: widget.isDense,
+              disabledHint: field.value != null
+                  ? Text("${field.value.toString()}")
+                  : widget.disabledHint,
+              elevation: widget.elevation,
+              iconSize: widget.iconSize,
+              icon: widget.icon,
+              iconDisabledColor: widget.iconDisabledColor,
+              iconEnabledColor: widget.iconEnabledColor,
+              underline: widget.underline,
+              onChanged: _readOnly
+                  ? null
+                  : (value) {
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      field.didChange(value);
+                      if (widget.onChanged != null) widget.onChanged(value);
+                    },
+            ),
           ),
         );
       },
     );
   }
+
+  /*@override
+  Widget build(BuildContext context) {
+    _readOnly = (_formState?.readOnly == true) ? true : widget.readOnly;
+
+    return DropdownButtonFormField(
+      key: _fieldKey,
+      decoration: widget.decoration.copyWith(
+        enabled: _readOnly ? false : true,
+      ),
+      hint: widget.hint,
+      items: widget.items,
+      value: _initialValue,
+      onChanged: _readOnly
+          ? null
+          : (value) {
+        FocusScope.of(context).requestFocus(FocusNode());
+        if (widget.onChanged != null) widget.onChanged(value);
+      },
+      validator: (val) {
+        for (int i = 0; i < widget.validators.length; i++) {
+          if (widget.validators[i](val) != null)
+            return widget.validators[i](val);
+        }
+        return null;
+      },
+      onSaved: (val) {
+        if (widget.valueTransformer != null) {
+          var transformed = widget.valueTransformer(val);
+          _formState?.setAttributeValue(widget.attribute, transformed);
+        } else
+          _formState?.setAttributeValue(widget.attribute, val);
+      },
+    );
+  }*/
 }
