ffbc4e4b6463e1b1ca03d86268963ddec7df960c
Anthony Whitford <anthony@whitford.com>
2020-06-11 03:46:00 -0700

    Revised ImageSourceSheet to use the new Image Picker api, and added support for web platform. (#310)

--- lib/src/widgets/image_source_sheet.dart
+++ lib/src/widgets/image_source_sheet.dart
@@ -1,16 +1,46 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageSourceSheet extends StatelessWidget {
+  /// Callback when an image is selected.
+  ///
+  /// **Note**: This will work on web platform whereas [onImageSelected] will not.
+  final Function(Image) onImage;
+
+  /// Callback when an image is selected.
+  ///
+  /// **Warning**: This will _NOT_ work on web platform because [File] is not
+  /// available.
   final Function(File) onImageSelected;
 
-  ImageSourceSheet({@required this.onImageSelected});
+  ImageSourceSheet({
+    Key key,
+    this.onImage,
+    this.onImageSelected,
+  })  : assert(null != onImage || null != onImageSelected),
+        super(key: key);
 
-  void imageSelected(File image) async {
-    if (image != null) {
-      onImageSelected(image);
+  Future<void> _onPickImage(ImageSource source) async {
+    final imagePicker = ImagePicker();
+    final pickedFile = await imagePicker.getImage(source: source);
+    if (null != pickedFile) {
+      if (null != onImage) {
+        final image = Image.memory(await pickedFile.readAsBytes());
+        onImage(image);
+      }
+
+      if (null != onImageSelected) {
+        // Warning:  this will not work on the web platform because pickedFile
+        // will instead point to a network resource.
+        assert(!kIsWeb);
+
+        final imageFile = File(pickedFile.path);
+        assert(null != imageFile);
+        onImageSelected(imageFile);
+      }
     }
   }
 
@@ -20,22 +50,14 @@ class ImageSourceSheet extends StatelessWidget {
       child: Wrap(
         children: <Widget>[
           ListTile(
-            leading: Icon(Icons.camera_enhance),
+            leading: const Icon(Icons.camera_enhance),
             title: Text('Camera'),
-            onTap: () async {
-              File image =
-                  await ImagePicker.pickImage(source: ImageSource.camera);
-              imageSelected(image);
-            },
+            onTap: () => _onPickImage(ImageSource.camera),
           ),
           ListTile(
-            leading: Icon(Icons.image),
+            leading: const Icon(Icons.image),
             title: Text('Gallery'),
-            onTap: () async {
-              File image =
-                  await ImagePicker.pickImage(source: ImageSource.gallery);
-              imageSelected(image);
-            },
+            onTap: () => _onPickImage(ImageSource.gallery),
           )
         ],
       ),
