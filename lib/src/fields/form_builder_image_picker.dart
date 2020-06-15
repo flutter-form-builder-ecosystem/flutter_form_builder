3fd5414868c1b3155f66f75daa3761f85f0df969
Danvick Miller <danvickmiller@live.com>
2020-06-15 23:09:49 +0300

    Put additional attributes for ImagePicker

--- lib/src/fields/form_builder_image_picker.dart
+++ lib/src/fields/form_builder_image_picker.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_form_builder/src/widgets/image_source_sheet.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FormBuilderImagePicker extends FormBuilderField {
   final String attribute;
@@ -12,10 +13,31 @@ class FormBuilderImagePicker extends FormBuilderField {
   final FormFieldSetter onSaved;
   final InputDecoration decoration;
 
-  final double imageWidth;
-  final double imageHeight;
-  final EdgeInsets imageMargin;
+  final double previewWidth;
+  final double previewHeight;
+  final EdgeInsets previewMargin;
+
   final Color iconColor;
+
+  /// Optional maximum height of image; see [ImagePicker].
+  final double maxHeight;
+
+  /// Optional maximum width of image; see [ImagePicker].
+  final double maxWidth;
+
+  /// The imageQuality argument modifies the quality of the image, ranging from
+  /// 0-100 where 100 is the original/max quality. If imageQuality is null, the
+  /// image with the original quality will be returned. See [ImagePicker].
+  final int imageQuality;
+
+  /// Use preferredCameraDevice to specify the camera to use when the source is
+  /// `ImageSource.camera`. The preferredCameraDevice is ignored when source is
+  /// `ImageSource.gallery`. It is also ignored if the chosen camera is not
+  /// supported on the device. Defaults to `CameraDevice.rear`. See [ImagePicker].
+  final CameraDevice preferredCameraDevice;
+
+  final Function(Image) onImage;
+  final int maxImages;
 
   FormBuilderImagePicker({
     Key key,
@@ -24,14 +46,21 @@ class FormBuilderImagePicker extends FormBuilderField {
     this.validator,
     this.valueTransformer,
     this.onChanged,
-    this.imageWidth = 130,
-    this.imageHeight = 130,
-    this.imageMargin,
+    this.previewWidth = 130,
+    this.previewHeight = 130,
+    this.previewMargin,
     this.readOnly = false,
     this.onSaved,
     this.decoration = const InputDecoration(),
     this.iconColor,
-  }) : super(
+    this.maxHeight,
+    this.maxWidth,
+    this.imageQuality,
+    this.preferredCameraDevice = CameraDevice.rear,
+    this.onImage,
+    this.maxImages,
+  })  : assert(maxImages == null || maxImages >= 0),
+        super(
           key: key,
           initialValue: initialValue ?? [],
           attribute: attribute,
@@ -41,6 +70,9 @@ class FormBuilderImagePicker extends FormBuilderField {
           readOnly: readOnly,
           builder: (FormFieldState field) {
             final _FormBuilderImagePickerState state = field;
+            final theme = Theme.of(state.context);
+            final disabledColor = theme.disabledColor;
+            final primaryColor = theme.primaryColor;
 
             return InputDecorator(
               decoration: decoration.copyWith(
@@ -48,80 +80,81 @@ class FormBuilderImagePicker extends FormBuilderField {
                 errorText: field.errorText,
               ),
               child: Container(
-                height: imageHeight,
+                height: previewHeight,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
-                  children: (state.value ?? []).map<Widget>((item) {
-                    return Stack(
-                      alignment: Alignment.topRight,
-                      children: <Widget>[
-                        Container(
-                          width: imageWidth,
-                          height: imageHeight,
-                          margin: imageMargin,
-                          child: item is String
-                              ? Image.network(item, fit: BoxFit.cover)
-                              : Image.file(item, fit: BoxFit.cover),
-                        ),
-                        if (!state.readOnly)
-                          InkWell(
-                            onTap: () {
-                              state.didChange([...state.value]..remove(item));
-                            },
-                            child: Container(
-                              margin: EdgeInsets.all(3),
-                              decoration: BoxDecoration(
-                                color: Colors.grey.withOpacity(.7),
-                                shape: BoxShape.circle,
-                              ),
-                              alignment: Alignment.center,
-                              height: 22,
-                              width: 22,
-                              child: Icon(
-                                Icons.close,
-                                size: 18,
-                                color: Colors.white,
-                              ),
+                  children: [
+                    if (field.value != null)
+                      ...(field.value.map<Widget>((item) {
+                        return Stack(
+                          alignment: Alignment.topRight,
+                          children: <Widget>[
+                            Container(
+                              width: previewWidth,
+                              height: previewHeight,
+                              margin: previewMargin,
+                              child: item is String
+                                  ? Image.network(item, fit: BoxFit.cover)
+                                  : Image.file(item, fit: BoxFit.cover),
                             ),
-                          ),
-                      ],
-                    );
-                  }).toList()
-                    ..add(
+                            if (!state.readOnly)
+                              InkWell(
+                                onTap: () {
+                                  field.didChange(
+                                      [...field.value]..remove(item));
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.all(3),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(.7),
+                                    shape: BoxShape.circle,
+                                  ),
+                                  alignment: Alignment.center,
+                                  height: 22,
+                                  width: 22,
+                                  child: Icon(
+                                    Icons.close,
+                                    size: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                          ],
+                        );
+                      }).toList()),
+                    if (!state.readOnly && !state.hasMaxImages)
                       GestureDetector(
                         child: Container(
-                            width: imageWidth,
-                            height: imageHeight,
+                            width: previewWidth,
+                            height: previewHeight,
                             child: Icon(Icons.camera_enhance,
                                 color: state.readOnly
-                                    ? Theme.of(state.context).disabledColor
-                                    : iconColor ??
-                                        Theme.of(state.context).primaryColor),
+                                    ? disabledColor
+                                    : iconColor ?? primaryColor),
                             color: (state.readOnly
-                                    ? Theme.of(state.context).disabledColor
-                                    : (iconColor ??
-                                        Theme.of(state.context).primaryColor))
+                                    ? disabledColor
+                                    : iconColor ?? primaryColor)
                                 .withAlpha(50)),
-                        onTap: state.readOnly
-                            ? null
-                            : () {
-                                showModalBottomSheet(
-                                  context: state.context,
-                                  builder: (_) {
-                                    return ImageSourceSheet(
-                                      onImageSelected: (image) {
-                                        if (image != null) {
-                                          state.didChange(
-                                              [...(state.value ?? []), image]);
-                                        }
-                                        Navigator.of(state.context).pop();
-                                      },
-                                    );
-                                  },
-                                );
-                              },
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: state.context,
+                            builder: (_) {
+                              return ImageSourceSheet(
+                                maxHeight: maxHeight,
+                                maxWidth: maxWidth,
+                                imageQuality: imageQuality,
+                                preferredCameraDevice: preferredCameraDevice,
+                                onImageSelected: (image) {
+                                  field
+                                      .didChange([...field.value ?? [], image]);
+                                  Navigator.of(state.context).pop();
+                                },
+                              );
+                            },
+                          );
+                        },
                       ),
-                    ),
+                  ],
                 ),
               ),
             );
@@ -134,4 +167,12 @@ class FormBuilderImagePicker extends FormBuilderField {
 
 class _FormBuilderImagePickerState extends FormBuilderFieldState {
   FormBuilderImagePicker get widget => super.widget;
+
+  bool get hasMaxImages {
+    if (widget.maxImages == null || value == null) {
+      return false;
+    } else {
+      return value.length >= widget.maxImages;
+    }
+  }
 }
