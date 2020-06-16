ae06509e632bc0ca0295165c4924b7e811f0b691
Danvick Miller <danvickmiller@live.com>
2020-06-16 15:44:51 +0300

    ColorPicker show Hex code in TextField instead of Color.toString()

--- lib/src/fields/form_builder_color_picker.dart
+++ lib/src/fields/form_builder_color_picker.dart
@@ -1,10 +1,28 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+extension on Color {
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${red.toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${green.toRadixString(16).padLeft(2, '0').toUpperCase()}'
+      '${blue.toRadixString(16).padLeft(2, '0').toUpperCase()}';
+}
 
 enum ColorPickerType { ColorPicker, MaterialPicker, BlockPicker }
 
@@ -141,7 +159,7 @@ class _FormBuilderColorPickerFieldState extends FormBuilderFieldState<Color> {
 
   FocusNode get effectiveFocusNode => _effectiveFocusNode;
 
-  String get valueString => value?.toString();
+  String get valueString => value?.toHex();
 
   Color _selectedColor;
 
