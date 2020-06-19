d307aa6a85b2769da4d079e3ede5925a70c0e24b
Danvick Miller <danvickmiller@live.com>
2020-06-19 20:09:29 +0300

    Started working on Example - break into smaller pieces, show code

---
+++ example/lib/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:example/code_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'sources/complete_form.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Form Builder'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Complete Form'),
+            trailing: Icon(CupertinoIcons.right_chevron),
+            onTap: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => CodePage(
+              title: 'Complete Form',
+              child: CompleteForm(),
+              sourceFilePath: 'lib/sources/complete_form.dart',
+            ))),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
