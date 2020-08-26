500833b74ec0651b73b5f1c40ba3adf27860edc1
vin-fandemand <64597233+vin-fandemand@users.noreply.github.com>
2020-08-26 15:13:31 -0400

    Fix for label overflows in Radio Groups (#431)
    * #317 fix for initializing signature
    
    * resolve review comments
    
    * fix final review comments
    
    * v3.11.3 docs
    
    * add txtstyle params and re-implmenet numberFormat (#343)
    
    * add txtstyle params and re-implmenet numberFormat
    
    * rename fieldTextStyle and add it as a fallback
    
    * Fix bug in ChoiceChip & FilterChip where using FieldOption label instead of child  breaks app. Closes #348
    
    * Added labelPadding and labelStyle to ChoiceChip field
    
    * Added const keyword to constructor.
    
    * Added an extra deprecated annotation so that users are alerted to the deprecation of label.  (I noticed that my code was not alerting with the deprecation only on the field declaration.)
    
    * Dependency upgrades.
    
    * chages to fix teh overflow with labels
    
    Co-authored-by: Danvick Miller <danvickmiller@live.com>
    Co-authored-by: William Cunha Cardoso <31575772+WilliamCunhaCardoso@users.noreply.github.com>
    Co-authored-by: Anthony Whitford <anthony@whitford.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -368,12 +368,20 @@ class MyHomePageState extends State<MyHomePage> {
                   ),
                   SizedBox(height: 15),
                   FormBuilderRadioGroup(
+                    orientation: GroupedRadioOrientation.wrap,
                     decoration:
                         InputDecoration(labelText: 'My chosen language'),
                     attribute: 'best_language',
                     onChanged: _onChanged,
                     validators: [FormBuilderValidators.required()],
-                    options: ['Dart', 'Kotlin', 'Java', 'Swift', 'Objective-C']
+                    options: [
+                      'Dart',
+                      'Kotlin',
+                      'Java',
+                      'Swift',
+                      'Objective-C',
+                      'A Very long text that would overflow to the next line and crash'
+                    ]
                         .map((lang) => FormBuilderFieldOption(
                               value: lang,
                               child: Text('$lang'),
@@ -562,7 +570,7 @@ class MyHomePageState extends State<MyHomePage> {
                 ],
               ),
             ),
-            Row(
+            ButtonBar(
               children: <Widget>[
                 Expanded(
                   child: MaterialButton(
@@ -583,17 +591,16 @@ class MyHomePageState extends State<MyHomePage> {
                 ),
                 SizedBox(width: 20),
                 Expanded(
-                  child: MaterialButton(
-                    color: Theme.of(context).accentColor,
-                    child: Text(
-                      'Reset',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      _fbKey.currentState.reset();
-                    },
+                    child: MaterialButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text(
+                    'Reset',
+                    style: TextStyle(color: Colors.white),
                   ),
-                ),
+                  onPressed: () {
+                    _fbKey.currentState.reset();
+                  },
+                )),
               ],
             ),
           ],
