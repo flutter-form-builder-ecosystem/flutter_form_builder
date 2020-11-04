c7ff558a292d57233c8f6075817866e16a432fc6
Anthony Whitford <anthony@whitford.com>
2020-11-04 00:16:36 -0800

    Elaborated the Dropdown field test.

--- test/fields/form_builder_dropdown_test.dart
+++ test/fields/form_builder_dropdown_test.dart
@@ -4,28 +4,24 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'form_builder_tester.dart';
 
 void main() {
-  testWidgets('FormBuilderDropdown -- 1,3', (WidgetTester tester) async {
+  testWidgets('FormBuilderDropdown -- 3,1', (WidgetTester tester) async {
     const widgetName = 'd1';
     final testWidget = FormBuilderDropdown<int>(
       name: widgetName,
-      items: [
+      items: const [
         DropdownMenuItem(
-          key: ValueKey(1),
           value: 1,
           child: Text('One'),
         ),
         DropdownMenuItem(
-          key: ValueKey(2),
           value: 2,
           child: Text('Two'),
         ),
         DropdownMenuItem(
-          key: ValueKey(3),
           value: 3,
           child: Text('Three'),
         ),
       ],
-      onChanged: (newValue) => print('>>>> Changed to $newValue'),
     );
     final widgetFinder = find.byWidget(testWidget);
     await tester.pumpWidget(buildTestableFieldWidget(testWidget));
@@ -34,20 +30,16 @@ void main() {
     expect(formValue(widgetName), isNull);
     await tester.tap(widgetFinder);
     await tester.pumpAndSettle();
-    await tester.tap(
-        find.descendant(of: widgetFinder, matching: find.byKey(ValueKey(1))));
-    await tester.pumpAndSettle();
-    expect(formSave(), isTrue);
-    expect(formValue(widgetName), equals(1));
-
-    /* Not sure why this isn't working; instead of 3, 1 is being tapped?
-    await tester.tap(widgetFinder);
-    await tester.pumpAndSettle();
-    await tester.tap(
-        find.descendant(of: widgetFinder, matching: find.byKey(ValueKey(3))));
+    await tester.tap(find.text('Three').last);
     await tester.pumpAndSettle();
     expect(formSave(), isTrue);
     expect(formValue(widgetName), equals(3));
-    */
+
+    await tester.tap(find.text('Three').last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('One').last);
+    await tester.pumpAndSettle();
+    expect(formSave(), isTrue);
+    expect(formValue(widgetName), equals(1));
   });
 }
