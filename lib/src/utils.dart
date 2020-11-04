f4c374a3c19d1063f3f5fb0ac0d69be4bc1c9b32
Anthony Whitford <anthony@whitford.com>
2020-11-04 11:39:56 -0800

    Replaced enumValueToString with describeEnum.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,10 +1,8 @@
-String enumValueToString(Object o) {
-  return o.toString().split('.').last;
-}
+import 'package:flutter/foundation.dart';
 
 T enumValueFromString<T>(String key, Iterable<T> values) {
   return values.firstWhere(
-    (v) => v != null && key == enumValueToString(v),
+    (v) => v != null && key == describeEnum(v),
     orElse: () => null,
   );
 }
