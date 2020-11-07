8bb47df6aaa855c77eda52589ef45794c0c41ea8
Anthony Whitford <anthony@whitford.com>
2020-11-07 01:33:30 -0800

    Removed unused import.

--- example/lib/sources/complete_form.dart
+++ example/lib/sources/complete_form.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:intl/intl.dart';
