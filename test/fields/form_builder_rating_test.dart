d2fa07d0b9605fa053d79f8b2a7030d07c387d6c
Anthony Whitford <anthony@whitford.com>
2020-11-07 20:26:25 -0800

    Added test coverage to FormBuilderRating for a read-only variant.

--- test/fields/form_builder_rating_test.dart
+++ test/fields/form_builder_rating_test.dart
@@ -3,10 +3,11 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'form_builder_tester.dart';
 
 void main() {
-  testWidgets('FormBuilderRating -- 3', (WidgetTester tester) async {
+  testWidgets('FormBuilderRating -- enabled', (WidgetTester tester) async {
     const widgetName = 'r1';
     final testWidget = FormBuilderRating(
       name: widgetName,
+      initialValue: 1.0,
     );
     await tester.pumpWidget(buildTestableFieldWidget(testWidget));
 
@@ -16,5 +17,22 @@ void main() {
     await tester.pumpAndSettle();
     expect(formSave(), isTrue);
     expect(formValue(widgetName), equals(3.0));
+  });
+
+  testWidgets('FormBuilderRating -- readOnly', (WidgetTester tester) async {
+    const widgetName = 'r2';
+    final testWidget = FormBuilderRating(
+      name: widgetName,
+      initialValue: 2.0,
+      enabled: false,
+    );
+    await tester.pumpWidget(buildTestableFieldWidget(testWidget));
+
+    expect(formSave(), isTrue);
+    expect(formValue(widgetName), equals(2.0));
+    await tester.tap(find.byWidget(testWidget));
+    await tester.pumpAndSettle();
+    expect(formSave(), isTrue);
+    expect(formValue(widgetName), equals(2.0));
   });
 }
