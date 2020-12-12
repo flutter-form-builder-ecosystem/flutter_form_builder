d0c4526ab70253130b708f96fa806ea7f506275c
Anthony Whitford <anthony@whitford.com>
2020-12-11 21:17:07 -0800

    Removed the key attribute because it was messing with focus.

--- lib/src/fields/form_builder_chips_input.dart
+++ lib/src/fields/form_builder_chips_input.dart
@@ -74,7 +74,6 @@ class FormBuilderChipsInput<T> extends FormBuilderField<List<T>> {
             final state = field as _FormBuilderChipsInputState<T>;
 
             return ChipsInput<T>(
-              key: ObjectKey(state.value),
               initialValue: field.value,
               enabled: state.enabled,
               decoration: state.decoration(),
