d6972e37758ce11c70edc035b3417ef513461bef
Alban DEFLANDRE <alban.deflandre@gmail.com>
2021-03-09 23:50:03 +0100

    Remove input using dependencies

--- lib/flutter_form_builder.dart
+++ lib/flutter_form_builder.dart
@@ -1,15 +1,12 @@
 library flutter_form_builder;
 
-export 'package:dropdown_search/dropdown_search.dart' hide ErrorBuilder;
-
 export './localization/form_builder_localizations.dart';
 export './src/fields/form_builder_checkbox.dart';
 export './src/fields/form_builder_checkbox_group.dart';
-export './src/fields/form_builder_chips_input.dart';
 export './src/fields/form_builder_dropdown.dart';
+export './src/fields/form_builder_filter_chips.dart';
 export './src/fields/form_builder_radio_group.dart';
 export './src/fields/form_builder_range_slider.dart';
-export './src/fields/form_builder_searchable_dropdown.dart';
 export './src/fields/form_builder_segmented_control.dart';
 export './src/fields/form_builder_slider.dart';
 export './src/fields/form_builder_switch.dart';
