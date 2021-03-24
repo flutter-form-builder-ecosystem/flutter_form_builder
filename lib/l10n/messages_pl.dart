f3eb4b418b238d77ad3010bf0dad28b26ab82a24
Danvick Miller <danvickmiller@live.com>
2021-03-24 18:29:44 +0300

    Fix bug in polish translations

--- lib/l10n/messages_pl.dart
+++ lib/l10n/messages_pl.dart
@@ -27,31 +27,33 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static m3(min) => "Wartość musi być większa lub równa ${min}.";
 
-  static m4(minLength) => "Wartość musi wynosić co najmniej ${minLength} znaków.";
+  static m4(minLength) =>
+      "Wartość musi wynosić co najmniej ${minLength} znaków.";
 
   static m5(value) => "Wartość tego pola nie może być ${value}.";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
+
   static _notInlinedMessages(_) => <String, Function>{
-    "creditCardErrorText": MessageLookupByLibrary.simpleMessage(
-        "To pole wymaga podania ważnego numeru karty kredytowej."),
-    "DateStringErrorText": MessageLookupByLibrary.simpleMessage(
-        "To pole wymaga poprawnego ciągu znaków daty."),
-    "emailErrorText": MessageLookupByLibrary.simpleMessage(
-        "To pole wymaga podania poprawnego adresu e-mail.")
-    "ipErrorText": MessageLookupByLibrary.simpleMessage(
-        "To pole wymaga ważnego IP."),
-    "matchErrorText": MessageLookupByLibrary.simpleMessage(
-        "Wartość nie pasuje do wzorca."),
-    "maxErrorText": m0,
-    "MaxLengthErrorText": m1,
-    "minErrorText": m2,
-    "MinLengthErrorText": m3,
-    "numericErrorText":
-    MessageLookupByLibrary.simpleMessage("Wartość musi być numeryczna."),
-    "requiredErrorText":
-    MessageLookupByLibrary.simpleMessage("To pole nie może być puste."),
-    "urlErrorText": MessageLookupByLibrary.simpleMessage(
-        "To pole wymaga podania poprawnego adresu URL.")
-  };
+        "creditCardErrorText": MessageLookupByLibrary.simpleMessage(
+            "To pole wymaga podania ważnego numeru karty kredytowej."),
+        "DateStringErrorText": MessageLookupByLibrary.simpleMessage(
+            "To pole wymaga poprawnego ciągu znaków daty."),
+        "emailErrorText": MessageLookupByLibrary.simpleMessage(
+            "To pole wymaga podania poprawnego adresu e-mail."),
+        "ipErrorText":
+            MessageLookupByLibrary.simpleMessage("To pole wymaga ważnego IP."),
+        "matchErrorText": MessageLookupByLibrary.simpleMessage(
+            "Wartość nie pasuje do wzorca."),
+        "maxErrorText": m0,
+        "MaxLengthErrorText": m1,
+        "minErrorText": m2,
+        "MinLengthErrorText": m3,
+        "numericErrorText": MessageLookupByLibrary.simpleMessage(
+            "Wartość musi być numeryczna."),
+        "requiredErrorText":
+            MessageLookupByLibrary.simpleMessage("To pole nie może być puste."),
+        "urlErrorText": MessageLookupByLibrary.simpleMessage(
+            "To pole wymaga podania poprawnego adresu URL.")
+      };
 }
