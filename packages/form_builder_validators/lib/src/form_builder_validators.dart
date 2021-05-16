2057c426f953b2aa675408c5b7030faa4b53be5d
Danvick Miller <danvickmiller@live.com>
2021-05-16 23:03:02 +0300

    Minor code formatting

--- packages/form_builder_validators/lib/src/form_builder_validators.dart
+++ packages/form_builder_validators/lib/src/form_builder_validators.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:form_builder_validators/src/utils/validators.dart';
 
-
 /// For creation of [FormFieldValidator]s.
 class FormBuilderValidators {
   /// [FormFieldValidator] that is composed of other [FormFieldValidator]s.
