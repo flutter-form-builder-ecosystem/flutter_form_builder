8cb61971bc5e90f4009d8ea2bbe6440fe91a08c9
Danvick Miller <danvickmiller@live.com>
2021-05-16 23:02:44 +0300

    Added form_builder_extra_fields package

--- packages/form_builder_extra_fields/lib/form_builder_extra_fields.dart
+++ packages/form_builder_extra_fields/lib/form_builder_extra_fields.dart
@@ -1,7 +1,9 @@
 library form_builder_extra_fields;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/fields/form_builder_chips_input.dart';
+export 'src/fields/form_builder_color_picker.dart';
+export 'src/fields/form_builder_rating.dart';
+export 'src/fields/form_builder_searchable_dropdown.dart';
+export 'src/fields/form_builder_signature_pad.dart';
+export 'src/fields/form_builder_touch_spin.dart';
+export 'src/fields/form_builder_typeahead.dart';
