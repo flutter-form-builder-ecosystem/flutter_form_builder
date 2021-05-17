8e78395d4247dffc06f18db86ee02fe5346a2bf2
Danvick Miller <danvickmiller@live.com>
2021-05-17 20:33:12 +0300

    Added CupertinoTextField to core_package example

--- packages/flutter_form_builder/example/lib/main.dart
+++ packages/flutter_form_builder/example/lib/main.dart
@@ -34,83 +34,118 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('FormBuilder Example'),
       ),
-      body: Column(
-        children: <Widget>[
-          FormBuilder(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                FormBuilderField<String>(
-                  name: "option",
-                  validator: (valueCandidate) {
-                    if (valueCandidate == null ||
-                        (valueCandidate is String && valueCandidate.isEmpty) ||
-                        (valueCandidate is Iterable &&
-                            valueCandidate.isEmpty) ||
-                        (valueCandidate is Map && valueCandidate.isEmpty)) {
-                      return 'This field is required.';
-                    }
-                    return null;
-                  },
-                  builder: (FormFieldState<String?> field) {
-                    return InputDecorator(
-                      decoration: InputDecoration(
-                        labelText: "Select option",
-                        contentPadding: EdgeInsets.only(top: 10.0, bottom: 0.0),
-                        border: InputBorder.none,
-                        errorText: field.errorText,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              FormBuilderField<String?>(
+                name: 'name',
+                onChanged: (val) => print(val),
+                builder: (FormFieldState field) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Text('Name'),
+                        flex: 1,
                       ),
-                      child: Container(
-                        height: 200,
-                        child: CupertinoPicker(
-                          itemExtent: 30,
-                          children: options.map((c) => Text(c)).toList(),
-                          onSelectedItemChanged: (index) {
-                            field.didChange(options[index]);
-                          },
+                      Expanded(
+                        flex: 2,
+                        child: InputDecorator(
+                          decoration: InputDecoration(
+                              errorText: field.errorText,
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.zero),
+                          child: CupertinoTextField(
+                            onChanged: (value) => field.didChange(value),
+                          ),
                         ),
                       ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: MaterialButton(
-                  color: Theme.of(context).accentColor,
-                  child: Text(
-                    "Submit",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    _formKey.currentState!.save();
-                    if (_formKey.currentState!.validate()) {
-                      print(_formKey.currentState!.value);
-                    } else {
-                      print("validation failed");
-                    }
-                  },
-                ),
+                    ],
+                  );
+                },
+                autovalidateMode: AutovalidateMode.always,
+                validator: (valueCandidate) {
+                  if (valueCandidate == null ||
+                      (valueCandidate is String && valueCandidate.isEmpty) ||
+                      (valueCandidate is Iterable && valueCandidate.isEmpty) ||
+                      (valueCandidate is Map && valueCandidate.isEmpty)) {
+                    return 'This field is required.';
+                  }
+                  return null;
+                },
               ),
-              SizedBox(width: 20),
-              Expanded(
-                child: MaterialButton(
-                  color: Theme.of(context).accentColor,
-                  child: Text(
-                    "Reset",
-                    style: TextStyle(color: Colors.white),
+              FormBuilderField<String>(
+                name: "option",
+                validator: (valueCandidate) {
+                  if (valueCandidate == null ||
+                      (valueCandidate is String && valueCandidate.isEmpty) ||
+                      (valueCandidate is Iterable && valueCandidate.isEmpty) ||
+                      (valueCandidate is Map && valueCandidate.isEmpty)) {
+                    return 'This field is required.';
+                  }
+                  return null;
+                },
+                builder: (FormFieldState<String?> field) {
+                  return InputDecorator(
+                    decoration: InputDecoration(
+                      labelText: "Select option",
+                      contentPadding: EdgeInsets.only(top: 10.0, bottom: 0.0),
+                      border: InputBorder.none,
+                      errorText: field.errorText,
+                    ),
+                    child: Container(
+                      height: 200,
+                      child: CupertinoPicker(
+                        itemExtent: 30,
+                        children: options.map((c) => Text(c)).toList(),
+                        onSelectedItemChanged: (index) {
+                          field.didChange(options[index]);
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MaterialButton(
+                      color: Theme.of(context).accentColor,
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        _formKey.currentState!.save();
+                        if (_formKey.currentState!.validate()) {
+                          print(_formKey.currentState!.value);
+                        } else {
+                          print("validation failed");
+                        }
+                      },
+                    ),
                   ),
-                  onPressed: () {
-                    _formKey.currentState!.reset();
-                  },
-                ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Theme.of(context).accentColor,
+                      child: Text(
+                        "Reset",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
