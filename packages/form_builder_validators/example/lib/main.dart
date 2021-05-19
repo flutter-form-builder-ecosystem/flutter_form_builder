d738dc63076aeaf4347f8cbf1ceeb146a744fec5
Danvick Miller <danvickmiller@live.com>
2021-05-19 11:37:24 +0300

    Add localization to validators example

--- packages/form_builder_validators/example/lib/main.dart
+++ packages/form_builder_validators/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
 void main() {
@@ -14,6 +15,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
+      supportedLocales: [
+        Locale('de'),
+        Locale('en'),
+        Locale('es'),
+        Locale('fr'),
+        Locale('it'),
+      ],
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        FormBuilderLocalizations.delegate,
+      ],
     );
   }
 }
@@ -39,18 +52,29 @@ class _MyHomePageState extends State<MyHomePage> {
               validator: FormBuilderValidators.required(context),
               autovalidateMode: AutovalidateMode.always,
             ),
+
+            // Composing multiple validators
             TextFormField(
               decoration: InputDecoration(labelText: 'Age'),
-              autovalidateMode: AutovalidateMode.always,
               keyboardType: TextInputType.number,
+              autovalidateMode: AutovalidateMode.always,
               validator: FormBuilderValidators.compose([
+                /// Makes this field required
+                FormBuilderValidators.required(context),
+
+                /// Ensures the value entered is numeric - with custom error message
                 FormBuilderValidators.numeric(context,
                     errorText: 'La edad debe ser numérica.'),
+
+                /// Sets a maximum value of 70
                 FormBuilderValidators.max(context, 70),
+
+                /// Include your own custom `FormFieldValidator` function, if you want
+                /// Ensures positive values only. We could also have used `FormBuilderValidators.min(context, 0)` instead
                 (val) {
-                  var number = double.tryParse(val ?? '');
-                  if (number != null) if (number < 0)
-                    return 'We cannot have a negative age';
+                  final number = int.tryParse(val);
+                  if (number == null) return null;
+                  if (number < 0) return 'We cannot have a negative age';
                   return null;
                 }
               ]),
