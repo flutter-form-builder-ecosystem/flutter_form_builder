3e582fa4bc22b94731b28ccc48321070ed9fd785
Danvick Miller <danvickmiller@live.com>
2021-05-20 13:56:45 +0300

    chore(fields): formatted example tests

--- packages/form_builder_fields/example/test/widget_test.dart
+++ packages/form_builder_fields/example/test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('FormBuilderTextField -- Hello Planet',
-          (WidgetTester tester) async {
-        expect(true, isTrue); // 😀
-      });
+      (WidgetTester tester) async {
+    expect(true, isTrue); // 😀
+  });
 }
