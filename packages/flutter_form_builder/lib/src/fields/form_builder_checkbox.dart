f03df122950bd91bf2ac0f5226f145b1842e7cbc
Danvick Miller <danvickmiller@live.com>
2021-11-11 12:36:54 +0300

    chore(core): fix unnecessary import errors causing CI fails in beta and dev workflows

--- packages/flutter_form_builder/lib/src/fields/form_builder_checkbox.dart
+++ packages/flutter_form_builder/lib/src/fields/form_builder_checkbox.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 /// Single Checkbox field
