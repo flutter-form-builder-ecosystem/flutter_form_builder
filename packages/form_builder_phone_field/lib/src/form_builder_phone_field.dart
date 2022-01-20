e3eb767c57575523c06ee91e825cfcb6ea8dac6a
William Cunha Cardoso <williamcardoso@protonmail.com>
2022-01-20 09:33:56 -0300

    chore: remove unused import

--- packages/form_builder_phone_field/lib/src/form_builder_phone_field.dart
+++ packages/form_builder_phone_field/lib/src/form_builder_phone_field.dart
@@ -4,7 +4,6 @@ import 'package:country_pickers/utils/typedefs.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:phone_number/phone_number.dart';
 
