e9633af7540da899b8ffe0d5afcaaf1c240fd774
Matias de Andrea <deandreamatias@gmail.com>
2023-05-04 09:10:51 +0200

    feat: improve examples

--- example/lib/code_page.dart
+++ example/lib/code_page.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 // import 'package:widget_with_codeview/widget_with_codeview.dart';
 
-class CodePage extends StatefulWidget {
+class CodePage extends StatelessWidget {
   final String title;
   final Widget child;
-  final String sourceFilePath;
 
   const CodePage({
-    Key? key,
+    super.key,
     required this.title,
     required this.child,
-    required this.sourceFilePath,
-  }) : super(key: key);
+  });
 
-  @override
-  State<CodePage> createState() => _CodePageState();
-}
-
-class _CodePageState extends State<CodePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0,
+      appBar: AppBar(title: Text(title), elevation: 0),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: child,
       ),
-      body: widget.child,
     );
   }
 }
