7cef78e21c8a40d4c77e2844dd2bf7a5e96c6dc8
Matias de Andrea <deandreamatias@gmail.com>
2025-01-05 13:10:17 +0100

    feat: improve minimal code example

--- example/lib/minimal_code_example.dart
+++ example/lib/minimal_code_example.dart
@@ -37,26 +37,57 @@ class _ExamplePageState extends State<_ExamplePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FormBuilder(
-        key: _formKey,
-        child: Column(
-          children: [
-            FormBuilderTextField(
-              name: 'full_name',
-              decoration: const InputDecoration(labelText: 'Full Name'),
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.required(),
-              ]),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                _formKey.currentState?.saveAndValidate();
-                debugPrint(_formKey.currentState?.value.toString());
-              },
-              child: const Text('Print'),
-            )
-          ],
+      appBar: AppBar(title: const Text('Minimal code example')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: [
+              FormBuilderFilterChip<String>(
+                decoration: const InputDecoration(
+                  labelText: 'The language of my people',
+                  enabled: false,
+                ),
+                name: 'languages_filter',
+                selectedColor: Colors.red,
+                options: const [
+                  FormBuilderChipOption(
+                    value: 'Dart',
+                    avatar: CircleAvatar(child: Text('D')),
+                  ),
+                  FormBuilderChipOption(
+                    value: 'Kotlin',
+                    avatar: CircleAvatar(child: Text('K')),
+                  ),
+                  FormBuilderChipOption(
+                    value: 'Java',
+                    avatar: CircleAvatar(child: Text('J')),
+                  ),
+                  FormBuilderChipOption(
+                    value: 'Swift',
+                    avatar: CircleAvatar(child: Text('S')),
+                  ),
+                  FormBuilderChipOption(
+                    value: 'Objective-C',
+                    avatar: CircleAvatar(child: Text('O')),
+                  ),
+                ],
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.minLength(1),
+                  FormBuilderValidators.maxLength(3),
+                ]),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState?.saveAndValidate();
+                  debugPrint(_formKey.currentState?.value.toString());
+                },
+                child: const Text('Print'),
+              )
+            ],
+          ),
         ),
       ),
     );
