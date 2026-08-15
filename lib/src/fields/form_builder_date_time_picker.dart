aa8f17dff5b975c5843f2139f399a6ba99b11938
Matias de Andrea <deandreamatias@gmail.com>
2026-08-15 16:41:35 +0200

    feat: migrate to material_ui

--- lib/src/fields/form_builder_date_time_picker.dart
+++ lib/src/fields/form_builder_date_time_picker.dart
@@ -1,6 +1,6 @@
 import 'dart:ui' as ui;
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter/services.dart';
 
 import 'package:intl/intl.dart';
--- lib/src/fields/form_builder_radio_group.dart
+++ lib/src/fields/form_builder_radio_group.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_form_builder/src/form_builder_field.dart';
 import 'package:flutter_form_builder/src/form_builder_field_decoration.dart';
 import 'package:flutter_form_builder/src/form_builder_field_option.dart';
